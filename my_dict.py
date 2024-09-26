a8d93ba078f8b64913e029129c4898296bde9b44
vladden60 <vladden60@gmail.com>
2024-09-26 14:04:56 +0300

    Add files via upload

---
+++ my_dict.py
@@ -0,0 +1,8 @@
+def func_my():
+    '''    
+    DOCSRING: There will be a description of the function here
+    INPUT: входных данных нет
+    OUTPUT: Привет!
+    '''
+    print('Привет!')
+help(func_my)
